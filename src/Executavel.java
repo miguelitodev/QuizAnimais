cf3fe5de5762c32ecdece29c0f9791a3617111ab
Miguel Riquelme <48338011+miguelrisquelme@users.noreply.github.com>
2019-10-01 23:22:15 -0300

    Estrutura Tela Questões

--- src/Executavel.java
+++ src/Executavel.java
@@ -1,12 +1,8 @@
 public class Executavel {
     public static void main(String[] args) {
         new Questoes().setVisible(true);
-
         /*
         new Pontuacao().setVisible(true);
-
-
-
         Carregamento loading = new Carregamento();
         if(loading.valor == 1){
             menu.setVisible(true);
--- src/Questoes.java
+++ src/Questoes.java
@@ -1,19 +1,17 @@
-import javafx.scene.control.RadioButton;
-
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ItemEvent;
-import java.awt.event.ItemListener;
-import java.awt.event.KeyEvent;
 
-public class Questoes extends JFrame  implements ItemListener{
+public class Questoes extends JFrame{
+
+    ImageIcon img = new ImageIcon("../img/Zebra.jgp");
+
+    //Instanciando os objetos
     public JRadioButton alternativa01, alternativa02, alternativa03, alternativa04;
     public JLabel pergunta, imagem;
-    public ButtonGroup radioGroup;
-    public JPanel painel1, painel2;
 
     public Questoes(){
 
+        //Criando a tela
         setTitle("Questões");
         setSize(400,400);
         setDefaultCloseOperation(DISPOSE_ON_CLOSE);
@@ -21,52 +19,37 @@ public class Questoes extends JFrame  implements ItemListener{
         setResizable(false);
         getContentPane().setBackground(new Color(178,178,255));
 
-        pergunta = new JLabel("Qual o animal:");
+        //Configurando e posicionando os objetos/componentes
         imagem = new JLabel("imagem");
+        //imagem = new JLabel(imagem.setIcon(img));
+        imagem.setBounds(0, 0, 256, 256);
 
-        painel1 = new JPanel();         painel2 = new JPanel();
+        pergunta = new JLabel("Qual o animal da imagem?");
+        pergunta.setBounds(100, 200, 200, 20);
 
         alternativa01 = new JRadioButton("alternativa 01");
+        alternativa01.setBounds(100, 225, 200, 20);
+        alternativa01.setBackground(new Color(178,178,255));
+
         alternativa02 = new JRadioButton("alternativa 02");
+        alternativa02.setBounds(100, 250, 200, 20);
+        alternativa02.setBackground(new Color(178,178,255));
+
         alternativa03 = new JRadioButton("alternativa 03");
+        alternativa03.setBounds(100, 275, 200, 20);
+        alternativa03.setBackground(new Color(178,178,255));
+
         alternativa04 = new JRadioButton("alternativa 04");
+        alternativa04.setBounds(100, 300, 200, 20);
+        alternativa04.setBackground(new Color(178,178,255));
 
-        alternativa01.setMnemonic(KeyEvent.VK_1);
-        alternativa02.setMnemonic(KeyEvent.VK_2);
-        alternativa03.setMnemonic(KeyEvent.VK_3);
-        alternativa04.setMnemonic(KeyEvent.VK_4);
-
-        radioGroup = new ButtonGroup();
-
-        radioGroup.add(alternativa01); alternativa01.addItemListener(this);
-        radioGroup.add(alternativa02); alternativa02.addItemListener(this);
-        radioGroup.add(alternativa03); alternativa03.addItemListener(this);
-        radioGroup.add(alternativa04); alternativa04.addItemListener(this);
-
-        painel1.setLayout(new FlowLayout(FlowLayout.RIGHT));
-        painel1.setBackground(new Color(200,200,200));
-
-        painel2.setLayout(new FlowLayout(FlowLayout.CENTER));
-        painel2.setBackground(new Color(200,200,200));
-
-
-        painel1.add(alternativa01);
-        painel1.add(alternativa02);
-        painel1.add(alternativa03);
-        painel1.add(alternativa04);
-
-        painel2.add(pergunta);
-        painel2.add(imagem);
-
-
-        getContentPane().add(painel1);
-        getContentPane().add(painel2);
-
-
-    }
-
-    @Override
-    public void itemStateChanged(ItemEvent e) {
-
+        // COlocando na tela os objetos/componentes
+        getContentPane().setLayout(null);
+        getContentPane().add(imagem);
+        getContentPane().add(pergunta);
+        getContentPane().add(alternativa01);
+        getContentPane().add(alternativa02);
+        getContentPane().add(alternativa03);
+        getContentPane().add(alternativa04);
     }
 }
