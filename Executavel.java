0061203a0bd8c90f5977382319518fc75bcffeb2
marcos <55929744+lilraffweed@users.noreply.github.com>
2019-10-05 15:04:54 -0300

    Add files via upload

---
+++ Executavel.java
@@ -0,0 +1,15 @@
+public class Executavel {
+    public static void main(String[] args) {
+    	Carregamento loading = new Carregamento();
+    	loading.setVisible(true);
+    	/*new Menu().setVisible(true);
+        /*
+        new Pontuacao().setVisible(true);
+        Carregamento loading = new Carregamento();
+        if(loading.valor == 1){
+            menu.setVisible(true);
+            loading.dispose();
+        }
+        */
+    }
+}
