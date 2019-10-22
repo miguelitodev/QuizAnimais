2a6991e04475510b1bad8563bb1c9bf6d0154f92
Hunter_Ytger <nicolasmtn@hotmail.com>
2019-10-22 16:45:02 -0300

    Alteração na class executavel

--- src/Executavel.java
+++ src/Executavel.java
@@ -1,8 +1,6 @@
 public class Executavel {
     public static void main(String[] args) {
     	// Chamou a tela menu, para iniciar o jogo
-        //
-        // new Menu().setVisible(true);
-        new Dev().setVisible(true);
+        new Menu().setVisible(true);
     }
 }
